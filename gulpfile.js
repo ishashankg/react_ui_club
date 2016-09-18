1208cb3e6abf6c1f47684b1b211fbf9fd1e209b4
Shashank Gupta <guptashashank15@gmail.com>
2016-09-18 19:59:20 +0530

    Uglify Js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
 
 gulp.task('default', function(){
 	return browserify('./source/app.js')
@@ -10,5 +12,6 @@ gulp.task('default', function(){
 			}))
 			.bundle()
 			.pipe(source('main.js'))
+			.pipe(streamify(uglify()))
 			.pipe(gulp.dest('./build/'));
 });
