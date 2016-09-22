22751be242c912739eef86b922a29f478ed53ba7
Shashank Gupta <shashank.gupta@talentica.com>
2016-09-22 21:18:08 +0530

    Prop validation

--- source/app.js
+++ source/app.js
@@ -2,8 +2,10 @@ var React = require('react');
 var ReactDOM  = require('react-dom');
 var reactClass = require('./components/ReactComponentState');
 var TimerExample = require('./components/Timer');
+var PropValidation = require('./components/PropValidation');
 
 var reactComponentElement = React.createElement(reactClass);
 ReactDOM.render(reactComponentElement, document.getElementById('react-application'));
 
-ReactDOM.render(  <TimerExample start={Date.now()} />, document.getElementById('timer'));
+var PropValidationElement = React.createElement(PropValidation);
+ReactDOM.render(PropValidationElement, document.getElementById('react-application'));
